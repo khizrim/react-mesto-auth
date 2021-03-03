14ab3b7abbbdec14f3d32eb51f34c370780cce8b
khizrim <kh.makhmudov@me.com>
2021-03-03 14:32:38 +0300

    refactor: improve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,42 +4,44 @@ import { Switch, Route, Link } from 'react-router-dom';
 function Header(props) {
   return (
     <header className='header'>
-      <a href='/' className='header__link' target='_self'>
-        <img src={logo} alt='Логотип' className='header__logo' />
-      </a>
-      <nav className='header__menu'>
-        <ul className='header__menu-links'>
-          <Switch>
-            <Route path='/sign-up'>
-              <li className='header__menu-item'>
-                <Link to='/sign-in' className='header__menu-link'>
-                  Войти
-                </Link>
-              </li>
-            </Route>
-            <Route path='/sign-in'>
-              <li className='header__menu-item'>
-                <Link to='/sign-up' className='header__menu-link'>
-                  Регистрация
-                </Link>
-              </li>
-            </Route>
-            {props.isLoggedIn ? (
-              <>
-                <li className='header__user-email'>{props.userEmail}</li>
-                <li
-                  onClick={props.onSignOut}
-                  className='header__menu-link header__menu-link_dimmed'
-                >
-                  Выйти
+      <div className='header__content'>
+        <a href='/' className='header__link' target='_self'>
+          <img src={logo} alt='Логотип' className='header__logo' />
+        </a>
+        <nav className='header__menu'>
+          <ul className='header__menu-links'>
+            <Switch>
+              <Route path='/sign-up'>
+                <li className='header__menu-item'>
+                  <Link to='/sign-in' className='header__menu-link'>
+                    Войти
+                  </Link>
                 </li>
-              </>
-            ) : (
-              ''
-            )}
-          </Switch>
-        </ul>
-      </nav>
+              </Route>
+              <Route path='/sign-in'>
+                <li className='header__menu-item'>
+                  <Link to='/sign-up' className='header__menu-link'>
+                    Регистрация
+                  </Link>
+                </li>
+              </Route>
+              {props.isLoggedIn ? (
+                <>
+                  <li className='header__user-email'>{props.userEmail}</li>
+                  <li
+                    onClick={props.onSignOut}
+                    className='header__menu-link header__menu-link_dimmed'
+                  >
+                    Выйти
+                  </li>
+                </>
+              ) : (
+                ''
+              )}
+            </Switch>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
